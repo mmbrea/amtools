50c7543cf4b0cc45fd8c0fe88f8344469bd25777
Amber Dahlberg <mmbrea@hotmail.com>
2024-04-14 21:40:37 -0600

    galaxybook(lapptop) google auth shit.

---
+++ pythontogooglesheets.py
@@ -0,0 +1,51 @@
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def update_values(spreadsheet_id, range_name, value_input_option, _values):
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        values = [
+            [
+                # Cell values ...
+                [["A", "B"], ["C", "D"]],
+            ],
+            # Additional rows ...
+            [["A", "B"], ["C", "D"]],
+        ]
+        body = {"values": values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=spreadsheet_id,
+                range=range_name,
+                valueInputOption=value_input_option,
+                body=body,
+            )
+            .execute()
+        )
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+if __name__ == "__main__":
+    # Pass: spreadsheet_id,  range_name, value_input_option and  _values
+    update_values(
+        "1SLBBaYnyNJEYLRec0gmjtipa6lx1Ls09mqCSOm_j3JY",
+        "Sheet1!A1:C10",
+        "USER_ENTERED",
+        [["A", "B"], ["C", "D"]],
+    )
